355964c94cdf7d1596b2e6c86ed3660039e2787e
GenixE <106990876+GenixE@users.noreply.github.com>
2025-02-11 20:21:46 +0100

    Preparing for Language functionality

--- src/components/Space.jsx
+++ src/components/Space.jsx
@@ -83,32 +83,35 @@ export const Space = () => {
 
     return (
         <div className="flex flex-col items-center w-full mt-4">
-            <div className="max-w-4xl w-full bg-white shadow-lg rounded-lg p-6">
+            <div className="max-w-5xl w-full bg-white shadow-lg rounded-lg p-6">
                 {/* Space Name and Ratings */}
                 <div className="flex justify-between items-center mb-4">
                     <h1 className="text-3xl font-bold">{space.name}</h1>
-                    <div className="flex items-center">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="#149d80"
-                            className="size-6"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007
-                  5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527
-                  1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354
-                  7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273
-                  -4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433
-                  2.082-5.006z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
-                        <span className="ml-2 text-gray-700">
-                            {space.totalScore && space.countScore ? (space.totalScore / space.countScore).toFixed(1) : 'No ratings'}
-                        </span>
-                    </div>
+                    {/* Conditionally render ratings and star */}
+                    {(space.totalScore && space.countScore) ? (
+                        <div className="flex items-center">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="#149d80"
+                                className="size-6"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007
+          5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527
+          1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354
+          7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273
+          -4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433
+          2.082-5.006z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            <span className="ml-2 text-gray-700">
+                {(space.totalScore / space.countScore).toFixed(1)}
+            </span>
+                        </div>
+                    ) : null}
                 </div>
 
                 {/* Photo */}
@@ -266,7 +269,7 @@ export const Space = () => {
                                     title="Google Maps Embed"
                                     width="100%"
                                     height="100%"
-                                    style={{ border: 0 }}
+                                    style={{border: 0}}
                                     src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${encodeURIComponent(
                                         space.name
                                     )}`}
@@ -277,7 +280,7 @@ export const Space = () => {
                             {/* Address Text and Get Directions Button */}
                             <div className="mt-4 flex flex-col sm:flex-row items-start sm:items-center justify-between">
                                 <p className="text-sm text-gray-600 mb-2 sm:mb-0">
-                                    {space.address.name}
+                                    {space.address.name}, {space.address?.municipality?.name}
                                 </p>
                                 <a
                                     href={`https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(
